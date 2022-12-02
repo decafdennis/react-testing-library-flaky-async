b59d6c81b0657b18475dffefc8a5ccf3b4ab1258
Dennis Stevense <decafdennis@gmail.com>
2022-12-02 22:08:27 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [cascadedCount, setCascadedCount] = useState(count);
+  
+  useEffect(() => {
+    console.log('effect', count);
+    setCascadedCount(count);
+  }, [count]);
+
+  console.log('render', count, cascadedCount);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <span data-testid="count">{count}</span>
+      <span data-testid="cascaded-count">{cascadedCount}</span>
+      <button onClick={() => {
+        setTimeout(() => {
+          setCount((count) => count + 1)
+        });
+      }}>Increment</button>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("increment", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const count = screen.getByTestId("count");
+  const cascadedCount = screen.getByTestId("cascaded-count");
+  expect(count).toHaveTextContent("0");
+  expect(cascadedCount).toHaveTextContent("0");
+
+  const button = screen.getByText("Increment");
+  await userEvent.click(button);
+  // Solution 1: wait for the async behavior in act() so that the cascading render happens here.
+  // await act(async () => {
+  //   await new Promise(resolve => setTimeout(resolve));
+  // });
+  await waitFor(async () => {
+    expect(count).toHaveTextContent("1");
+  });
+  // Solution 2: yield to the next macrotask so that any batched renders can happen.
+  // await new Promise(resolve => setTimeout(resolve));
+  expect(cascadedCount).toHaveTextContent("1");
 });
